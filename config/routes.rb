4d2884f43b232a6509599551dd7d17bf38f18a8b
tony612 <h.bing612@gmail.com>
2013-06-19 12:56:00 +0800

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 BondService::Application.routes.draw do
+  get "dashboard/index"
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: "dashboard#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -39,7 +41,7 @@ BondService::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
