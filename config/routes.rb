2ba7f1ea349363ccade056f295a4f7e29d6735b2
tony612 <h.bing612@gmail.com>
2013-06-22 16:14:07 +0800

    fix the user routes bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 BondService::Application.routes.draw do
-  resources :users
   get "dashboard/index"
   devise_for :users
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
